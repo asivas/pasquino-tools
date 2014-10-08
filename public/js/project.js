7df2ddd8797997aabf16755bb5e9ba89507f9869
vidaguren <vidaguren>
2014-10-08 19:51:47 +0000

    seleccionador de carpetas

--- public/js/project.js
+++ public/js/project.js
@@ -1,9 +1,28 @@
 $(function(){
-	initModulo('dlgProyecto','Proyecto','Proyecto','filtroNombre','botonAlta','articles','proyecto');
+	var optionsInit = {
+			onLoad: function() {
+				if($('#fileManager').length==0)
+					$('input[name=ruta]').parent().append("<div id=\"fileManager\"></div>");
+				var fmOpts = {ajaxPath:'fileManager.php',
+						events:{
+					         click: function() {
+					            var data = $(this).data();
+					            $('input[name=ruta]').val(data.item.fullPath);
+					         }
+					      }
+				};
+				if($('input[name=ruta]').val()!='')
+					fmOpts.path = $('input[name=ruta]').val(); 
+				$('#fileManager').fileManager(fmOpts);				
+			}
+	}
+	initModulo('dlgProyecto','Proyecto','Proyecto','filtroNombre','botonAlta','articles','proyecto',optionsInit);
 	$('body').on('change','input#toggletodos',function(e){
 		var setChecked = $(this).prop('checked');
 		$("#tablas input[type=checkbox]").each(function() {
 				$(this).prop('checked',setChecked);
 				});			
 	});
+	
+	
 });
